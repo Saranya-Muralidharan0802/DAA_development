1b202b9133a6c5399037e9112bc7633156e3cbf6
Saranya-Muralidharan0802 <157654630+Saranya-Muralidharan0802@users.noreply.github.com>
2024-02-03 23:43:16 -0600

    added merge sort code

---
+++ merge_sort.py
@@ -0,0 +1,29 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+input_array = [5, 2, 4, 7, 1, 3, 2, 6]
+sorted_array = merge_sort(input_array)
+print("Sorted Array:", sorted_array)
